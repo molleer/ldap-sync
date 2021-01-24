f96801bb2da929b49f41b3dd407b1bad1be89a4c
David Möller <damoller99@hotmail.se>
2021-01-24 15:26:11 +0100

    Refactored ldap connection and login functionality

--- pkg/services/ldap.go
+++ pkg/services/ldap.go
@@ -1,5 +1,12 @@
 package services
 
+import (
+	"crypto/tls"
+
+	"github.com/spf13/viper"
+	"gopkg.in/ldap.v2"
+)
+
 type LdapConfig struct {
 	Url        string
 	ServerName string
@@ -9,4 +16,20 @@ type LdapConfig struct {
 type LoginConfig struct {
 	UserName string
 	Password string
+}
+
+func LDAPConnect() (*ldap.Conn, error) {
+	config := LdapConfig{
+		Url:        viper.GetString("ldap.url"),
+		ServerName: viper.GetString("ldap.servername"),
+		Tls:        viper.GetBool("ldap.tls"),
+	}
+
+	return ldap.DialTLS("tcp",
+		config.Url,
+		&tls.Config{ServerName: config.ServerName, InsecureSkipVerify: !config.Tls})
+}
+
+func LoginUser(l *ldap.Conn, config LoginConfig) error {
+	return l.Bind(config.UserName, config.Password)
 }
