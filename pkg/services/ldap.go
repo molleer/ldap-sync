37ab4d4d5bbbf840d2f3eaa1e050e52da3b9e12d
David Möller <davidm@student.chalmers.se>
2021-04-06 20:44:18 +0200

    Created function for adding new user

--- pkg/services/ldap.go
+++ pkg/services/ldap.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"crypto/tls"
+	"fmt"
+	"math/rand"
 	"strconv"
 
 	"github.com/spf13/viper"
@@ -112,4 +114,105 @@ func (s *ServiceLDAP) GetITUsers() ([]ITUser, error) {
 	}
 
 	return itUsers, nil
+}
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+func (s *ServiceLDAP) AddITUser(user ITUser) error {
+	/*
+		dn: uid=bill,ou=people,dc=chalmers,dc=it
+		changetype: add
+		accepteduseragreement: TRUE
+		admissionyear: 2001
+		cn: %{firstname} '%{nickname}' %{lastname}
+		gidnumber: 4500
+		givenname: Bill
+		homedirectory: /home/chalmersit/bill
+		loginshell: /bin/bash
+		mail: bill@student.chalmers.se
+		nickname: Billy
+		objectclass: chalmersstudent
+		objectclass: posixAccount
+		objectclass: top
+		preferredlanguage: sv
+		sn: Billysson
+		telephonenumber: 07212345667
+		uid: bill
+		uidnumber: 10000
+		userpassword: {SSHA}xeHcFuIwJ8R2JffkdS1YEgPUHzN67kvx
+	*/
+
+	gdpr := ""
+	if user.Gdpr {
+		gdpr = "TRUE"
+	} else {
+		gdpr = "FALSE"
+	}
+
+	return s.Connection.Add(&ldap.AddRequest{
+		DN: fmt.Sprintf("uid=%s,%s", user.Cid, s.UsersConfig.BaseDN),
+		Attributes: []ldap.Attribute{
+			{Type: "accepteduseragreement", Vals: []string{gdpr}},
+			{Type: "admissionyear", Vals: []string{strconv.FormatInt(int64(user.AcceptanceYear), 10)}},
+			{Type: "cn", Vals: []string{"%{firstname} '%{nickname}' %{lastname}"}},
+			{Type: "gidnumber", Vals: []string{"4500"}},
+			{Type: "givenname", Vals: []string{user.FirstName}},
+			{Type: "homedirectory", Vals: []string{fmt.Sprintf("/home/chalmersit/%s", user.Cid)}},
+			{Type: "loginshell", Vals: []string{"/bin/bash"}},
+			{Type: "mail", Vals: []string{user.Email}},
+			{Type: "nickname", Vals: []string{user.Nick}},
+			{Type: "objectclass", Vals: []string{"chalmersstudent", "posixAccount", "top"}},
+			{Type: "sn", Vals: []string{user.LastName}},
+			{Type: "telephonenumber", Vals: []string{user.Phone}},
+			{Type: "uid", Vals: []string{user.Cid}},
+			{Type: "uidnumber", Vals: []string{"10010"}},
+			{Type: "userpassword", Vals: []string{fmt.Sprintf("{SSHA}%s", RandomString(20))}},
+		},
+	})
+
+	/*request := ldap.NewSearchRequest(
+		s.UsersConfig.BaseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		s.UsersConfig.Filter,
+		[]string{"uid", "givenName", "sn", "acceptedUserAgreement", "admissionYear",
+			"nickname", "mail", "telephoneNumber", "preferredLanguage"},
+		nil,
+	)
+
+	users, err := s.Connection.Search(request)
+	if err != nil {
+		return nil, err
+	}
+
+	itUsers := []ITUser{}
+	for _, entry := range users.Entries {
+		userAgreement, _ := strconv.ParseBool(entry.GetAttributeValue("acceptedUserAgreement"))
+		admissionYear, _ := strconv.Atoi(entry.GetAttributeValue("admissionYear"))
+		itUsers = append(itUsers, ITUser{
+			Cid:                   entry.GetAttributeValue("uid"),
+			FirstName:             entry.GetAttributeValue("givenName"),
+			LastName:              entry.GetAttributeValue("sn"),
+			UserAgreement:         userAgreement,
+			AcceptanceYear:        admissionYear,
+			Nick:                  entry.GetAttributeValue("nickname"),
+			Email:                 entry.GetAttributeValue("mail"),
+			Phone:                 entry.GetAttributeValue("telephoneNumber"),
+			Language:              entry.GetAttributeValue("preferredLanguage"),
+			AccountLocked:         false,
+			Activated:             true,
+			Enabled:               true,
+			AccountNonLocked:      true,
+			CredentialsNonExpired: true,
+		})
+	}
+
+	return itUsers, nil*/
 }
--- pkg/tests/ldap_test.go
+++ pkg/tests/ldap_test.go
@@ -31,17 +31,31 @@ func TestMain(m *testing.M) {
 
 func TestConnection(t *testing.T) {
 	l, err := services.NewLDAPService()
-	assert.Equal(t, err, nil, "Failed to dial LDAP server")
+	assert.NoError(t, err, "Failed to dial LDAP server")
 	defer l.Connection.Close()
 
 	err = l.LoginUser(services.LoginConfig{
 		UserName: viper.GetString("ldap.user"),
 		Password: viper.GetString("ldap.password"),
 	})
-	assert.Equal(t, err, nil, "Failed to login as admin")
+	assert.NoError(t, err, "Failed to login as admin")
 }
 
 func TestGetUsers(t *testing.T) {
 	_, err := service.GetITUsers()
-	assert.Equal(t, err, nil, "An error ocurred when fetching user")
+	assert.NoError(t, err, "An error ocurred when fetching user")
+}
+
+func TestAddUer(t *testing.T) {
+	err := service.AddITUser(services.ITUser{
+		Cid:            "wmacmak",
+		Gdpr:           true,
+		AcceptanceYear: 2002,
+		FirstName:      "Wyatt",
+		Email:          "wmacmak@student.chalmers.se",
+		LastName:       "MacMakin",
+		Nick:           "Chokladkaka",
+		Phone:          "123456789",
+	})
+	assert.NoError(t, err, "An error ocurred when adding users")
 }
